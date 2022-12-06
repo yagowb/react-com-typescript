30e4c842c34125a89b847eff5420739cf03d2cec
yago rodrigues <yagodev3@gmail.com>
2022-12-06 18:59:01 -0300

    .

--- components/Botao/index.tsx
+++ components/Botao/index.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import style from './Botao.module.scss';
 
-interface Props {
-  type?: "button" | "submit" | "reset" | undefined,
-  onClick?: () => void,
-  children?: React.ReactNode
-}
-
-function Botao({ onClick, type, children }: Props) {
-  return (
-    <button
-      onClick={onClick}
-      type={type}
-      className={style.botao}
-    >
-      {children}
-    </button>
-  )
+class Botao extends React.Component {
+  render() {
+    return (
+      <button>
+        Botão
+      </button>
+    )
+  }
 }
 
 export default Botao;
--- components/Lista/index.tsx
+++ components/Lista/index.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { ITarefa } from '../../types/tarefa';
-import Item from './Item';
-import style from './Lista.module.scss';
 
-interface Props {
-  tarefas: ITarefa[],
-  selecionaTarefa: (tarefaSelecionada: ITarefa) => void 
-}
-
-
-function Lista({ tarefas, selecionaTarefa }: Props) {
+function Lista() {
+  const tarefas = [{
+    tarefa: 'React',
+    tempo: '02:00:00'
+  }, {
+    tarefa: 'Javascript',
+    tempo: '01:00:00'
+  }, {
+    tarefa: "Typescript",
+    tempo: "03:00:00"
+  }];
   return (
-    <aside className={style.listaTarefas}>
+    <aside>
       <h2> Estudos do dia </h2>
       <ul>
-        {tarefas.map(item => (
-          <Item
-            selecionaTarefa={selecionaTarefa}
-            key={item.id}
-            {...item}
-          />
+        {tarefas.map((item, index) => (
+          <li key={index}>
+            <h3>{item.tarefa}</h3>
+            <span>{item.tempo}</span>
+          </li>
         ))}
       </ul>
     </aside>
